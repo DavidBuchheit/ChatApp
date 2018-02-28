c351b857018aa862a62cd61461e18f03c5d38ebb
David Buchheit <DavidBuchheit98@gmail.com>
2018-02-28 17:19:51 -0600

    Clearly defined how requests are gonna work

--- Server.py
+++ Server.py
@@ -43,30 +43,50 @@ def main(connectionSocket):
 
 
 # SendMessage \t Message \r\n
+# SendMessage \t Success \r\n
+# SendMessage \t Failure \r\n
 def sendMessage():
     print("sendMessage")
 
-# SendPrivateMessage \t Sender \t Receiver \t Message
+# SendPrivateMessage \t Sender \t Receiver \t Message \r\n
+# SendPrivateMessage \t Success \r\n
+# SendPrivateMessage \t Failure \r\n
 def sendPrivateMessage():
     print("sendPrivateMessage")
 
 # RegisterUser \t FirstName \t LastName \t Address \t Email \t password1 \t password2 \r\n
+# RegisterUser \t Success \r\n
+# RegisterUser \t Failure \r\n
 def RegisterUser():
     print("RegisterUser")
 
 # CheckLogin \t Username \t Password \r\n
+# CheckLogin \t Success \r\n
+# CheckLogin \t Failure \r\n
 def loginCheck():
     print("checkUser")
 
 # FriendsList \r\n
+# FriendsList \t Success \t Array Of Friends \r\n
+# ArraySchema: User, Status
+# FriendsList \t Failure \r\n
 def printFriendsList():
     print("printFriendsList")
 
+# FriendStatus \t Friend \r\n
+# FriendStatus \t Success \t Status \r\n
 # FriendStatus \t Requester \t Friend \r\n
 def getFriendStatus():
     print("getFriendStatus")
 
 # OfflineMessages \r\n
+# OfflineMessages \t Success \t Messages Array \r\n
+# ArraySchema: User, Message, Time
+# OfflineMessages \t Failure \r\n
+
+# OfflinePrivateMessages \t Success \t Messages Array \r\n
+# OfflinePrivateMessages \t Failure \t \r\n
+# ArraySchema: User, Message, Time
 def getOfflineMessages():
     print("getOfflineMessages")
 
