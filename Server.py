8f6ecd5073e5188ed0f6c4d6d3be1e7777b5cfb7
David Buchheit <DavidBuchheit98@gmail.com>
2018-02-27 23:49:06 -0600

    Function Definition

--- Server.py
+++ Server.py
@@ -1,21 +1,52 @@
 ###Chat Bot
 
 import sqlite3 as lite
+from socket import *
+
+#list = database.execute("SELECT * FROM User")
+# list = list.fetchall()
+# print(list)
+# Example SQL print
+
+serverPort = 12009
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(10)
 database = lite.connect('user.db')
 def main():
-    list = database.execute("SELECT * FROM User")
-    # list = list.fetchall()
-    # print(list)
-    # Example SQL print
+    print("main")
 
 
 
 
 
 
+def sendMessage():
+    print("sendMessage")
 
 
+def sendPrivateMessage():
+    print("sendPrivateMessage")
 
+
+def RegisterUser():
+    print("RegisterUser")
+
+
+def loginCheck():
+    print("checkUser")
+
+
+def printFriendsList():
+    print("printFriendsList")
+
+
+def getFriendStatus():
+    print("getFriendStatus")
+
+
+def getOfflineMessages():
+    print("getOfflineMessages")
 
 
 if __name__ == '__main__':
