46fcc3a1c0d8cc3a4bd187bc0ff91a8c56c7a559
David Buchheit <DavidBuchheit98@gmail.com>
2018-02-27 22:22:42 -0600

    First

---
+++ Server.py
@@ -0,0 +1,23 @@
+###Chat Bot
+
+import sqlite3 as lite
+import sys
+database = lite.connect('user.db')
+def main():
+    # list = database.execute("SELECT * FROM USERS")
+    # list = list.fetchall()
+    # print(list)
+    # Example SQL print
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
